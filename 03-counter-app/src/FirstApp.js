88d4f3d8989f7c047c18786f4eabed764c29b548
Luis Alberto Alvarez <smum15973@hotmail.com>
2022-04-06 13:30:10 -0600

    FirstApp component

---
+++ 03-counter-app/src/FirstApp.js
@@ -0,0 +1,9 @@
+// This is our first FC (functional component) this is the current way how we
+// create component in React. It is used to use classes instead functions, and
+// there could be the possiblity to still find those way of coding.
+const FirstApp = () => {
+
+    return <h1>Hello Friend</h1>
+}
+
+export default FirstApp;
