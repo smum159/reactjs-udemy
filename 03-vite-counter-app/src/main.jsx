afd282d1ec38e0b81438c00c60c08fcca91f5129
Luis Alberto Alvarez <smum15973@hotmail.com>
2022-09-01 13:07:31 -0600

    Setting up tests section

--- 03-vite-counter-app/src/main.jsx
+++ 03-vite-counter-app/src/main.jsx
@@ -5,13 +5,6 @@ import { FirstApp } from './02-first-app';
 import CounterApp from './03-counter-app';
 import './styles/index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//     <React.StrictMode>
-//         <HelloFriend/>
-//         <FirstApp/>
-//     </React.StrictMode>
-// )
-
 // Here we are referencing the 'root' div in the public/index.html file
 const divRoot = document.querySelector('#root');
 
