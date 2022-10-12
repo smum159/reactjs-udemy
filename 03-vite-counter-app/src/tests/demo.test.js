54d853f01283eac382efd2fe4632f599c8153b02
Luis Alberto Alvarez <smum15973@hotmail.com>
2022-10-12 13:11:14 -0600

    adding jest types

--- 03-vite-counter-app/src/tests/demo.test.js
+++ 03-vite-counter-app/src/tests/demo.test.js
@@ -1,6 +1,9 @@
 
-console.log("Hello from the test 👻");
+console.log("🤖🌮 Hello from the test 🤖🌮");
 
 test('Initial test', () => {
+   const msg1 = 'Hello friend...';
+   const msg2 = msg1.trim();
+
     
  })
